7535667c935902429fb030a207ed9c115a940922
Victor Rönneling <Victoronneling@gmail.com>
2024-12-01 10:31:57 +0100

    Add files via upload

---
+++ restic-backup.sh
@@ -0,0 +1,9 @@
+#you need to replace username and ip address with your own.
+RESTIC_REPOSITORY="sftp:user@ipaddress:/home/username/restic-backup"
+PASSWORD="test"
+#username replace with your own.
+BACKUP_SOURCE="/home/username/play-with-sockets"
+
+export PASSWORD
+
+restic -r "$RESTIC_REPOSITORY" backup "$BACKUP_SOURCE"
